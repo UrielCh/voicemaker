a0d86458d71e13be372292e2ada8670f866b5707
urielch <uriel.chemouni@gmail.com>
2024-02-04 06:07:49 +0100

    move main in a main condition

--- src/main.ts
+++ src/main.ts
@@ -50,113 +50,117 @@ interface SayOption {
   lang?: string;
 }
 
-const program = new Command();
+if (import.meta.main) {
+  const program = new Command();
 
-program
-  .version("1.1.0")
-  .command("list")
-  .arguments("[engine]")
-  .description("List available voices from an engine")
-  .option(
-    "-l, --lang <lang>",
-    "Display only voice with the correct lang code",
-    parseLangCode,
-  )
-  .action(async function (engine: string, options: SayOption) {
-    if (engine === "voicemaker") {
-      for (const voice of Object.keys(voiceMakerVoiceCache)) {
-        const lang = voiceMakerVoiceCache[voice as VoiceMakerVoices];
-        if (options.lang && !lang.includes(options.lang)) continue;
-        let dispVoice = voice.padEnd(20);
-        dispVoice = pc.yellow(dispVoice);
-        const effects = getVoicemakerEffect(voice);
-        console.log(
-          `${dispVoice} speak ${pc.green(lang.padEnd(6))} supported effect: ${
-            pc.red(effects.join(", "))
-          }`,
-        );
-      }
-      return;
-    }
-    if (engine === "google") {
-      for (const voice of Object.keys(GoogleTTS2Voice)) {
-        const info = GoogleTTS2Voice[voice as GoogleVoices];
-        const { lang } = options;
-        if (lang && !info.langs.find((curLang) => curLang.includes(lang))) {
-          continue;
+  program
+    .version("1.1.0")
+    .command("list")
+    .arguments("[engine]")
+    .description("List available voices from an engine")
+    .option(
+      "-l, --lang <lang>",
+      "Display only voice with the correct lang code",
+      parseLangCode,
+    )
+    .action(async function (engine: string, options: SayOption) {
+      if (engine === "voicemaker") {
+        for (const voice of Object.keys(voiceMakerVoiceCache)) {
+          const lang = voiceMakerVoiceCache[voice as VoiceMakerVoices];
+          if (options.lang && !lang.includes(options.lang)) continue;
+          let dispVoice = voice.padEnd(20);
+          dispVoice = pc.yellow(dispVoice);
+          const effects = getVoicemakerEffect(voice);
+          console.log(
+            `${dispVoice} speak ${pc.green(lang.padEnd(6))} supported effect: ${
+              pc.red(effects.join(", "))
+            }`,
+          );
         }
-        let langs = info.langs.join(", ");
-        langs = pc.green(langs);
-        let ssmlGender = info.ssmlGender.padEnd(6);
-        if (ssmlGender.startsWith("MALE")) {
-          ssmlGender = pc.cyan(ssmlGender);
-        } else {
-          ssmlGender = pc.red(ssmlGender);
+        return;
+      }
+      if (engine === "google") {
+        for (const voice of Object.keys(GoogleTTS2Voice)) {
+          const info = GoogleTTS2Voice[voice as GoogleVoices];
+          const { lang } = options;
+          if (lang && !info.langs.find((curLang) => curLang.includes(lang))) {
+            continue;
+          }
+          let langs = info.langs.join(", ");
+          langs = pc.green(langs);
+          let ssmlGender = info.ssmlGender.padEnd(6);
+          if (ssmlGender.startsWith("MALE")) {
+            ssmlGender = pc.cyan(ssmlGender);
+          } else {
+            ssmlGender = pc.red(ssmlGender);
+          }
+          let dispVoice = voice.padEnd(17);
+          dispVoice = pc.yellow(dispVoice);
+          console.log(`${dispVoice} is a ${ssmlGender} speaking ${langs}`);
         }
-        let dispVoice = voice.padEnd(17);
-        dispVoice = pc.yellow(dispVoice);
-        console.log(`${dispVoice} is a ${ssmlGender} speaking ${langs}`);
+        return;
       }
-      return;
-    }
-    if (engine === "watson") {
-      for (const voice of ALL_WATSON_VOICES) {
-        if (options.lang && !voice.startsWith(options.lang)) continue;
-        console.log(`${voice}`);
+      if (engine === "watson") {
+        for (const voice of ALL_WATSON_VOICES) {
+          if (options.lang && !voice.startsWith(options.lang)) continue;
+          console.log(`${voice}`);
+        }
+        return;
       }
-      return;
-    }
-    if (engine === "elevenlabs" || engine === "11labs") {
-      const provider = new ElevenLabs();
-      const voices = await provider.getVoices();
-      for (const voice of voices) {
-        // for (const lang of model.languages) {
-        //     if (options.lang && lang.language_id !== options.lang)
-        //         continue
-        //     console.log(`modelId: ${model.model_id} speak ${lang.language_id}}`)
-        // }
-        const voice_id = pc.yellow(voice.voice_id);
-        const name = pc.yellow(voice.name.padEnd(10));
-        const txt = `${voice_id} name: ${name} [${
-          pc.red(voice.category)
-        }] sample: ${pc.underline(voice.preview_url)}`;
-        console.log(txt);
+      if (engine === "elevenlabs" || engine === "11labs") {
+        const provider = new ElevenLabs();
+        const voices = await provider.getVoices();
+        for (const voice of voices) {
+          // for (const lang of model.languages) {
+          //     if (options.lang && lang.language_id !== options.lang)
+          //         continue
+          //     console.log(`modelId: ${model.model_id} speak ${lang.language_id}}`)
+          // }
+          const voice_id = pc.yellow(voice.voice_id);
+          const name = pc.yellow(voice.name.padEnd(10));
+          const txt = `${voice_id} name: ${name} [${
+            pc.red(voice.category)
+          }] sample: ${pc.underline(voice.preview_url)}`;
+          console.log(txt);
+        }
+        return;
       }
-      return;
-    }
-    console.error(`Engine parameter should be on of: ${ALL_ENGINE.join(", ")}`);
-    Deno.exit(-1);
-  });
-
-program
-  .command("say", { isDefault: true })
-  .arguments("<text...>")
-  .description("use a TTS engine to make your computer speak")
-  .option(
-    "-v, --voice <name>",
-    "select a voiceMaker, or a google cloud TTS voice",
-  )
-  .option("-s, --speed <speed>", "customise talk speed", parsePerCent)
-  .option("-p, --pitch <pitch>", "customise talk pitch", parsePerCent)
-  .option("-l, --lang <lang>", "specify a lang code", parseLangCode)
-  .option(
-    "-V, --volume <volume>",
-    "increate or decrease speech volume",
-    parsePerCent,
-  )
-  .action(async function (texts: string[], options: SayOption) {
-    const voice = getVoice(options.voice);
-    const engine = getEngine(voice.voice);
-    const req = engine.getRequest(texts.join(" "), voice.voice);
-    if (options.speed) req.speedPer100 = options.speed;
-    if (options.pitch) req.pitchPer100 = options.pitch;
-    if (options.volume) req.volumePer100 = options.volume;
-    try {
-      await engine.say(req);
-    } catch (e) {
-      console.error(e);
+      console.error(
+        `Engine parameter should be on of: ${ALL_ENGINE.join(", ")}`,
+      );
       Deno.exit(-1);
-    }
-  });
-// process.argv
-program.parse();
+    });
+
+  program
+    .command("say", { isDefault: true })
+    .arguments("<text...>")
+    .description("use a TTS engine to make your computer speak")
+    .option(
+      "-v, --voice <name>",
+      "select a voiceMaker, or a google cloud TTS voice",
+    )
+    .option("-s, --speed <speed>", "customise talk speed", parsePerCent)
+    .option("-p, --pitch <pitch>", "customise talk pitch", parsePerCent)
+    .option("-l, --lang <lang>", "specify a lang code", parseLangCode)
+    .option(
+      "-V, --volume <volume>",
+      "increate or decrease speech volume",
+      parsePerCent,
+    )
+    .action(async function (texts: string[], options: SayOption) {
+      const voice = getVoice(options.voice);
+      const engine = getEngine(voice.voice);
+      const req = engine.getRequest(texts.join(" "), voice.voice);
+      if (options.speed) req.speedPer100 = options.speed;
+      if (options.pitch) req.pitchPer100 = options.pitch;
+      if (options.volume) req.volumePer100 = options.volume;
+      try {
+        await engine.say(req);
+      } catch (e) {
+        console.error(e);
+        Deno.exit(-1);
+      }
+    });
+  // process.argv
+  program.parse();
+}
